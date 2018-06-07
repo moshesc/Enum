010ac1e6a7e399bc96a249c221f4e58b6e222c6a
moshesc <beepbeep9860@gmail.com>
2018-06-07 08:05:13 +0100

    Commit

---
+++ Enum/List.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Enum
+{
+    class List
+    {
+        enum Color
+        {
+            Red,
+            Green,
+            Blue
+        }
+        static void PrintColor(Color color)
+        {
+            switch (color)
+            {
+                case Color.Red:
+                    Console.WriteLine("Red");
+                    break;
+                case Color.Green:
+                    Console.WriteLine("Green");
+                    break;
+                case Color.Blue:
+                    Console.WriteLine("Blue");
+                    break;
+            }
+        }
+        static void Main()
+        {
+            Color c = Color.Red;
+            PrintColor(c);
+            PrintColor(Color.Blue);
+        }
+    }
+}
--- Enum/Program.cs
+++ Enum/Program.cs
@@ -4,9 +4,6 @@ namespace Enum
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        
     }
 }
